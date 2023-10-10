7c1f51558de2bdb289897ebfeace3ebf11f46b61
mendozam1335 <55033050+mendozam1335@users.noreply.github.com>
2023-10-09 21:28:46 -0500

    Implemented one question open at a time

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ import Questions from "./Questions";
 
 const App = () => {
   const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleQuestion = (id) => {
+    const newActiveID = activeId === id ? null : id;
+    setActiveId(newActiveID);
+  };
+
   return (
     <main>
-      <Questions questions={questions} />
+      <Questions
+        questions={questions}
+        activeId={activeId}
+        toggleQuestion={toggleQuestion}
+      />
     </main>
   );
 };
--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import SingleQuestion from "./SingleQuestion";
-const Questions = ({ questions }) => {
+const Questions = ({ questions, activeId, toggleQuestion }) => {
   return (
     <article className="container">
       <h1>Questions</h1>
       {questions.map((question) => {
-        return <SingleQuestion key={question.id} {...question} />;
+        return (
+          <SingleQuestion
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          />
+        );
       })}
     </article>
   );
